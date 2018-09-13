dc76315378a7f95a2ab62562040c4d9475058457
araujrafa <rafael.araujo@grupofolha.com.br>
2018-09-13 08:50:48 -0300

    Adicionado arquivos iniciais do projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const resposeApi = require('./api/fields.json');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/form.html'));
+});
+
+app.get('/api/fields.json', (req, res) => {
+  res.json(resposeApi);
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
